1d436f514115923d319b9cf1ff3eb487d20d223c
jroyals2 <jamesroyals3@gmail.com>
2017-11-08 08:15:26 -0500

    added the rest of the setup

--- server.js
+++ server.js
@@ -17,11 +17,12 @@ console.log('Mongoose default connection error: ' + err);
 });
 
 app.use(bodyParser.json());
+app.use(express.static(__dirname + '/client/build/'));
 app.get('/', (req,res) => {
-res.send('Hello world!')
-})
+    res.sendFile(__dirname + '/client/build/index.html')
+      })
 
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 3001;
 app.listen(PORT, () => {
 console.log("Magic happening on port " + PORT);
 })
